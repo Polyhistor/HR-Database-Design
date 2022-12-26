53056a1a49b646e71ca26830ac5d7a05ac9239e6
Pouya <pouya.ataei.7@gmail.com>
2022-12-26 22:31:58 +1300

    fixed all the issues with normalization, updated the DML

--- Postgress-DDLs.sql
+++ Postgress-DDLs.sql
@@ -57,13 +57,16 @@ CREATE TABLE Employee_History (
 -- DML
 
 INSERT INTO Department (name)
-SELECT DISTINCT department_nm FROM proj_stg;
+SELECT DISTINCT department_nm 
+FROM proj_stg;
 
 INSERT INTO Education (level) 
-SELECT DISTINCT education_lvl FROM proj_stg;
+SELECT DISTINCT education_lvl 
+FROM proj_stg;
 
 INSERT INTO City (name)
-SELECT DISTINCT city FROM proj_stg;
+SELECT DISTINCT city 
+FROM proj_stg;
 
 INSERT INTO Office (location, address, city_id)
 SELECT DISTINCT st.location, st.address, c.id
@@ -72,25 +75,34 @@ JOIN city AS c
 ON st.city = c.name;
 
 INSERT INTO Salary (amount)
-SELECT salary FROM proj_stg;
+SELECT salary 
+FROM proj_stg;
 
 INSERT INTO Job (title) 
-SELECT Distinct job_title FROM proj_stg;
+SELECT Distinct job_title 
+FROM proj_stg;
 
-INSERT INTO employee (id, name, email, education_id, department_id, salary_id, office_id, job_id, manager_id)
-SELECT st.emp_id, st.emp_nm, st.email, e.id, d.id, s.id, o.id, j.id, stm.emp_id 
-FROM proj_stg AS st  
-JOIN education AS e 
-ON st.education_lvl = e.level 
-JOIN department AS d 
+INSERT INTO employee(id, name, email)
+SELECT DISTINCT emp_id, emp_nm, email 
+FROM proj_stg;
+
+INSERT INTO employee_history(start_date, end_date, hire_date, employee_id, education_id, department_id, salary_id, manager_id, office_id, job_id)
+SELECT st.start_dt, st.end_dt, st.hire_dt, e.id, ed.id, d.id, s.id, st2.emp_id, o.id, j.id  
+FROM proj_stg AS st 
+JOIN employee AS e
+ON st.emp_id = e.id 
+JOIN education AS ed 
+ON st.education_lvl = ed.level 
+JOIN department AS d
 ON st.department_nm = d.name 
 JOIN salary AS s 
 ON st.salary = s.amount 
+JOIN proj_stg as st2
+ON st.manager = st2.emp_nm
 JOIN office AS o 
-ON st.location = o.location  
+ON st.location = o.location
 JOIN job AS j 
-ON st.job_title = j.title
-LEFT JOIN proj_stg AS stm
-ON stm.emp_nm = st.manager;
+ON st.job_title = j.title; 
+
 
 
